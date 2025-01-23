d3bc4e77966bfbe764b7e00db7d7173ee130ae00
Lucas Souza <outrolucas2002@gmail.com>
2025-01-23 18:02:16 -0300

    Fix any typing

--- src/components/admin/WinnerSelector.tsx
+++ src/components/admin/WinnerSelector.tsx
@@ -13,8 +13,9 @@ import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Button } from "@/components/ui/button";
 import { useToast } from "@/hooks/use-toast";
 import { redirect, useRouter } from "next/navigation";
+import { Category } from "@/server/api/zod/schema";
 
-export function WinnerSelector({ categories }: { categories: any }) {
+export function WinnerSelector({ categories }: { categories: Category[] }) {
   const [selectedCategory, setSelectedCategory] = useState("");
   const [selectedNomination, setSelectedNomination] = useState("");
   const { toast } = useToast();
