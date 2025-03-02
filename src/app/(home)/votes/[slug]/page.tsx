af66562c023539e2551a11e231b7c5f9b1c81f6e
Lucas Felipe <outrolucas2002@gmail.com>
2025-03-02 19:35:28 -0300

    NO TIME FOR COMMIT MESSAGES

--- src/app/(home)/votes/[slug]/page.tsx
+++ src/app/(home)/votes/[slug]/page.tsx
@@ -3,7 +3,6 @@ import { Suspense } from "react";
 import { VotePageContent } from "./VotePageContent";
 import { api } from "@/trpc/server";
 import VotesPageSkeleton from "@/components/votes/VotesPageSkeleton";
-import { redirect } from "next/navigation";
 
 export const dynamicParams = false;
 
@@ -16,11 +15,6 @@ export const generateStaticParams = async () => {
 
 const VotePage = async ({ params }: { params: { slug: string } }) => {
   const categories = await api.nominations.getCategories({ ascending: false });
-  const winningNominations = await api.nominations.getWinningNominations();
-
-  if (winningNominations.length !== 0) {
-    redirect("/");
-  }
 
   return (
     <Suspense fallback={<VotesPageSkeleton />}>
